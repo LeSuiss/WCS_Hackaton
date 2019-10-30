41fc0971c3df5a3e9b5387db9cd96993c3a7d49c
Ouahid Aqodad <aqodad.ouahid26@gmail.com>
2019-10-30 17:17:48 +0100

    add movies page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import './App.css';
+import Footer from './components/footer/Footer'
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "bootstrap-css-only/css/bootstrap.min.css";
+import "mdbreact/dist/css/mdb.css";
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Footer />
     </div>
   );
 }
