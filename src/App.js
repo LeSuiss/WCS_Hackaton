3eec3d4b54e3b0ccd8ad79594e32d773440efe9b
AlexG <alexguidat@gmail.com>
2019-10-30 21:53:37 +0100

    ajout router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import Footer from './components/footer/Footer'
+import Api from './components/api/Api'
 
 function App() {
   return (
-    <div>
-      <Footer />
-
+    <div className="App">
+      <Api/>
+      <Router />
     </div>
   );
 }
