e4f3aa15a18b36a6a3b6a4d9125030290044a2c9
Ouahid Aqodad <aqodad.ouahid26@gmail.com>
2019-10-30 13:40:47 +0100

    add card component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import RecipeReviewCard from './components/Card'
 
 function App() {
   return (
     <div className="App">
+      <RecipeReviewCard/>
     </div>
   );
 }
