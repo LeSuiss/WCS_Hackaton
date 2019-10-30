fd7d674891ece1068674b560a7c9113beaac6f79
Anthony Portier <anthonynicolasportier@gmail.com>
2019-10-30 12:49:57 +0100

    start api

---
+++ src/components/api/Api.jsx
@@ -0,0 +1,11 @@
+class Api extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default Api;
